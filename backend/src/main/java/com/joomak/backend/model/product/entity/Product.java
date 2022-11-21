de3d524cc334ee905b4050a09baf4153e5e0b360
hwlee0103 <sdiverHyeWon0103@gmail.com>
2022-11-21 22:12:51 +0900

    Product Entity - Builder Pattern

--- backend/src/main/java/com/joomak/backend/model/product/entity/Product.java
+++ backend/src/main/java/com/joomak/backend/model/product/entity/Product.java
@@ -2,10 +2,8 @@ package com.joomak.backend.model.product.entity;
 
 import com.joomak.backend.model.common.BaseEntity;
 import com.joomak.backend.model.product.enums.ProductState;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import com.sun.istack.NotNull;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,18 +11,21 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "Product")
 @Getter
-//@Builder
 @EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id", updatable = false)
+    @NotNull
     private Long id;
 
+    @NotNull
     @Column(name = "product_name")
     private String productName;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(name = "product_state")
     private ProductState productState;
@@ -32,21 +33,25 @@ public class Product extends BaseEntity {
     @Column(name = "product_description")
     private String productDescription;
 
+    @NotNull
     @Column(name = "product_price")
     private Integer productPrice;
 
     @Column(name = "product_image_path")
     private String productImagePath;
 
+    @NotNull
     @Column(name = "is_sale")
     private Boolean isSale;
 
+    @NotNull
     @Column(name = "is_show")
     private Boolean isShow;
 
     @Column(name = "origin_place")
     private String originPlace;
 
+    @NotNull
     @Column(name = "stock")
     private Integer stock;
 
@@ -56,4 +61,23 @@ public class Product extends BaseEntity {
     @Column(name = "max_order_quantity")
     private Integer maxOrderQuantity;
 
+    //기본 생성자?
+    @Builder
+    public Product(Long id, String productName, ProductState productState,
+                   String productDescription, Integer productPrice, String productImagePath,
+                   Boolean isSale, Boolean isShow, String originPlace, Integer stock,
+                   Integer minOrderQuantity, Integer maxOrderQuantity) {
+        this.id = id;
+        this.productName = productName;
+        this.productState = productState;
+        this.productDescription = productDescription;
+        this.productPrice = productPrice;
+        this.productImagePath = productImagePath;
+        this.isSale = isSale;
+        this.isShow = isShow;
+        this.originPlace = originPlace;
+        this.stock = stock;
+        this.minOrderQuantity = minOrderQuantity;
+        this.maxOrderQuantity = maxOrderQuantity;
+    }
 }
